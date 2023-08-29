fae42d46fa8350ffb57e6a6157d84ac1c8c4462f
Yaroslav Soromlya <yaroslav.soromlya@ralabs.org>
2023-08-29 10:51:20 +0300

    Add textarea

--- src/pages/OrderForm/OrderForm.tsx
+++ src/pages/OrderForm/OrderForm.tsx
@@ -1,14 +1,22 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import TextField from "@mui/material/TextField";
 import { Typography } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs, { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Switch from "@mui/material/Switch";
+import Textarea from "@mui/joy/Textarea";
 
 import styles from "./OrderForm.module.scss";
 
 const OrderForm: FC = () => {
+  const [isSpecial, setIsSpecial] = useState<boolean>(false);
+
+  const handleChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsSpecial(e.target.checked);
+  };
+
   return (
     <div className={styles.container}>
       <Typography component={"h4"} variant="h6" className={styles.title}>
@@ -28,7 +36,6 @@ const OrderForm: FC = () => {
             <TextField id="first-name" size="small" />
           </div>
         </div>
-
         <div className={styles.inputRow}>
           <div className={styles.inputElement}>
             <label htmlFor="phone">Phone:</label>
@@ -42,13 +49,28 @@ const OrderForm: FC = () => {
                 defaultValue={dayjs("2023-09-02")}
                 disablePast
                 className={styles.datePicker}
-                slotProps={{ textField: { size: 'small', id: 'datepicker' } }}
+                slotProps={{ textField: { size: "small", id: "datepicker" } }}
               />
             </LocalizationProvider>
           </div>
-
-
         </div>
+
+        <div className={styles.switchRow}>
+          <label htmlFor="special">Special:</label>
+          <Switch
+            id="special"
+            checked={isSpecial}
+            onChange={handleChecked}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+        </div>
+
+
+        
+
+        {isSpecial && <Textarea minRows={6} placeholder="Your comments" />}
+
+        {/* comments Delivery method Payment method */}
       </div>
     </div>
   );
